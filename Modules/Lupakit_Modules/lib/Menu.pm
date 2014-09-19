0902331fa26cd90e3be3151895a4e5b290626a59
donixfree <donatello.caccamo@gmail.com>
2014-09-19 10:36:33 +0200

    New Update

--- Modules/Lupakit_Modules/lib/Menu.pm
+++ Modules/Lupakit_Modules/lib/Menu.pm
@@ -7,17 +7,14 @@ sub menu_principale {
 
 my $menu = << "MAIN_MENU";
     
-    ---------  LUPAKIT 1.0 ---------------
+   +-------------  LUPAKIT 1.0 ------------------+
 
     (1)   Password Generator
     
     (2)   Equazioni: Serie di Fibonacci
          
           Hosts List:
-          
-          (I)nsert (R)eplace 
-          
-          (D)elete <-in progress 
+          (i)nsert (r)eplace (d)elete 
           
     (3)   Change Remote Password  <-in progress
     (4)   Copy   Remote Key (SSH) <-in progress
@@ -29,8 +26,8 @@ my $menu = << "MAIN_MENU";
     (9)   Report Remote SCRIPT    <-in progress
 
     
-                         by d0n1x, luma
-    -------------------------------------
+                                    by d0n1x, luma
+   +----------------------------------------------+
 
 MAIN_MENU
 
