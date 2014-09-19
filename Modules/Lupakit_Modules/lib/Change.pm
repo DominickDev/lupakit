e4569df55f086719003d5ded092b9dc4b3d665f3
donixfree <donatello.caccamo@gmail.com>
2014-09-19 14:44:16 +0200

    update Expect.pm

--- Modules/Lupakit_Modules/lib/Change.pm
+++ Modules/Lupakit_Modules/lib/Change.pm
@@ -2,21 +2,76 @@
 
 package Change;
 
-sub change_password {
-
-}
-
-sub change_user_add {
-
-}
-
-sub change_user_remove {
-
-}
-
 sub change_copy_key {
+    #---------------------------------------------------------
+    # ABSTRACT:  
+    # SSH Login Without Password Using ssh-keygen & ssh-copy-id  
+    #---------------------------------------------------------  
+      
+    #INPUT
+    my $filename_input = "hosts.txt";
+    my @hosts_array;
+    my $input_flag;
+    my $path_pub_key;
+
+    #PROCESS
+    my $host
+  
+    #OUTPUT 
+  
+
+    #Richiesta esistenza chiave pubblica e chiave privata su localhost:
+    print "\n Hai una chiave pubblica e privata su localhost? (y/n):\n";
+    $input_flag = <>;
+    chomp $input_flag;
+    
+    #Generazione chiavi pubblica e privata rsa:
+    system(ssh-key-gen) if $input_flag eq "n";
+    
+    #Copia file hosts.txt nell'array @host_array per successivo uso:
+    open(FH_READ_HOSTS, ">$filename_input")
+            or die "Error while opening file:$!\n";
+            
+            @hosts_array = <FH_READ_HOSTS>;
+            chomp @host_array;
+    
+    close(FH_READ_HOSTS)
+            or die "Error while closing file:$!\n";
+
+   
+    #Ciclo copia key per ogni hosts
+    foreach (@hosts_array) {
+    
+    system(ssh-id
+    
+    #Login (static password)
+    Expect::my_expect();
+
+    #
+    scp 
 
 }
+
+
+
+
+
+
+
+
+
+
+#sub change_password {
+
+#}
+
+#sub change_user_add {
+
+#}
+
+#sub change_user_remove {
+
+#}
 
 
 
--- Modules/Lupakit_Modules/lib/Expect.pm
+++ Modules/Lupakit_Modules/lib/Expect.pm
@@ -2,7 +2,75 @@
 
 package Expect;
 
-sub expect_ssh_copy_key {
+use Expect;
+#$Expect::Debug = 1;
+use strict;
+use warnings;
+
+sub expect_copy_key {
+
+    #---------------------------------------------
+    #
+    # ABSTRACT:
+    # Copy SSH pubblic key on remote host by automatic login.
+    # Expect module is required.
+    #
+    #---------------------------------------------
+
+    #-----------------------------------------------
+    #INPUT
+    my $timeout = '30';
+    
+    #Questi parametri verranno letti in futuro da un file locale secret.txt...in progress
+    my $username = 'pippo';
+    my $oldpass = 'password_default';
+    my $newpass = 'Gtn_547_0ghr';
+    
+    #PROCESS
+
+    #OUTPUT
+    #------------------------------------------------
+
+    Readlist::read_host();
+
+    my $exp = Expect->spawn("ssh-copy-id -i -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username\@$hostname")
+        or die "Cannot spawn ssh: $!\n";
+
+    my $spawn_ok;
+
+    $exp->expect($timeout,
+             [ qr /\@.*password: $/ => sub {
+                                $spawn_ok = 1;
+                                my $expect = shift;
+                                $expect->send("${oldpass}\n");
+                                exp_continue;} ],
+             [ qr /\(current\) UNIX password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${oldpass}\n");
+                                exp_continue;} ],
+             [ qr /New password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${newpass}\n");
+                                exp_continue;} ],
+             [ qr /Retype new password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${newpass}\n");
+                                exp_continue;}],
+        [ eof => sub {
+                if ($spawn_ok) {
+                        die "ERROR: premature EOF in login.\n";}
+                else {
+                        die "ERROR: could not spawn telnet.\n";}}],
+        [ timeout => sub {die "No login.\n";}],
+
+        '-re', qr'[#>:] $', #' wait for shell prompt, then exit expect
+        );
+        
+
+
+}
+
+sub expect_ssh {
 
     use Expect;
 
