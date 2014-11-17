a24d3014cfa8d093d4759216842daef274d6d294
donix <donixfree@gmail.com>
2014-11-17 17:58:20 +0100

    Creato 2 pezzi di codice separati su esperimenti/

---
+++ esperimenti/Change.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+#use lib $Bin.'/Modules/lib/';
+#use FindBin qw/$Bin/;
+
+use 5.20.1;
+
+use lib '/home/donix/script/progetti/lupakit/esperimenti/Modules/lib';
+
+use Accesso;
+
+
+	my $username  = 'pippo';
+	my $oldpass = 'perldipippo';
+	my $newpass = 'adessosonobravo';
+	my $host = '10.0.3.214';
+
+
+Accesso::cambia_password($username, $oldpass, $newpass, $host);
+
+
+
+
+
+
---
+++ esperimenti/Modules/lib/Accesso.pm
@@ -0,0 +1,55 @@
+#!/usr/bin/perl;
+
+package Accesso;
+    use Expect;
+    $Expect::Debug = 1;
+    use strict;
+    use warnings;
+
+sub cambia_password {
+
+#    use Expect;
+
+
+    my ($username, $oldpass, $newpass, $host) = shift;
+
+    my $timeout = '30';
+
+    #Collegamento SSH a $host e cambio password scaduta con una nuova:
+    my $exp = Expect->spawn("ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username\@$host")
+        or die "Cannot spawn ssh: $!\n";
+
+    my $spawn_ok;
+
+    $exp->expect($timeout,
+             [ qr /\@.*password: $/ => sub {
+                                $spawn_ok = 1;
+                                my $expect = shift;
+                                $expect->send("${oldpass}\n");
+                                exp_continue;} ],
+             [ qr /\(current\) UNIX password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${oldpass}\n");
+                                exp_continue;} ],
+             [ qr /New password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${newpass}\n");
+                                exp_continue;} ],
+             [ qr /Retype new password:/ => sub {
+                                my $expect = shift;
+                                $expect->send("${newpass}\n");
+                                exp_continue;}],
+        [ eof => sub {
+                if ($spawn_ok) {
+                        die "ERROR: premature EOF in login.\n";}
+                else {
+                        die "ERROR: could not spawn telnet.\n";}}],
+        [ timeout => sub {die "No login.\n";}],
+
+        '-re', qr'[#>:] $', #' wait for shell prompt, then exit expect
+        );
+
+
+}
+
+1;
